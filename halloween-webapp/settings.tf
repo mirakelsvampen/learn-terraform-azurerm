56a09ff9bb71f5560e09746af888f6d0b0dce0df
Ben Kooijman <ben.kooijman@protonmail.com>
2020-10-20 08:50:06 +0200

    Initial commit

---
+++ halloween-webapp/settings.tf
@@ -0,0 +1,15 @@
+terraform {
+    required_providers {
+        azurerm = {
+            source = "hashicorp/azurerm"
+            version = "~> 2.28.0"
+        }
+    }
+    backend "azurerm" {
+    }
+}
+
+provider "azurerm" {
+    skip_provider_registration = true
+    features {}
+}
